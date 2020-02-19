adff7ece68636dfd3efb317f6b3c6aae7fa6fac4
Mayank Rikh <mayankrikh@gmail.com>
2020-02-19 18:35:17 +0000

    Added networking

---
+++ Front-End/hackathon/Utilities/Networking/APIManager + EndPoints.swift
@@ -0,0 +1,25 @@
+//
+//  APIManager + EndPoints.swift
+//  
+//
+//  Created by Mayank Rikh on 25/01/19.
+//  Copyright © 2019 Mayank Rikh. All rights reserved.
+//
+import Foundation
+
+extension APIManager {
+
+    enum EndPoint : String {
+
+        case getFireList
+        
+        var path : String {
+
+            let url = ""
+            return url + rawValue
+        }
+    }
+}
+
+
+
---
+++ Front-End/hackathon/Utilities/Networking/APIManager.swift
@@ -0,0 +1,26 @@
+//
+//  APIManager.swift
+//  
+//
+//  Created by Mayank Rikh on 25/01/19.
+//  Copyright © 2019 Mayank Rikh. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class APIManager {
+
+    static func getFireList(params : [String : Any], completion: ((JSON?, Error?)->())?){
+
+        NetworkingManager.GET(endPoint: .getFireList, parameters: params, headers : headers, success: { (dict) in
+            completion?(JSON(dict), nil)
+        }) { (error) in
+            completion?(nil, error)
+        }
+    }
+
+    static var headers : [String : String]{
+        return ["":""]
+    }
+}
