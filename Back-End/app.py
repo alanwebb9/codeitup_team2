b5d13fbee2b591cb8e30c31cb854542e978fe015
Parna Salian <parna146@gmail.com>
2020-02-19 14:42:19 +0000

    Adding hospital and garda details

--- Back-End/app.py
+++ Back-End/app.py
@@ -3,16 +3,38 @@ import requests, csv
 import mysql.connector
 import os
 import fireDetails
-
+import database
+import hospitalDetails,gardaDetails
 app = Flask(__name__)
 
-@app.route("/getFireDetails", methods =['GET'])
+dbconn = database.db
+cursor = dbconn.cursor()
+csv_data = csv.reader(open('Garda_Stations.csv'))
+for row in csv_data:
+    cursor.execute("INSERT into gardaDetails (Name, Address1, Address2,Address3, Phone, Website, Division,Divisional_HQ,Divisional_HQ_Phone,District,District_HQ,District_HQ_Phone,Opening_Hours,latitude,longitude)values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", row)
+dbconn.commit()
+cursor.close()
+print("Done")
+
+@app.route("/getFireList", methods =['GET'])
 def getFireDetails():
     fireDetailsDictionary = {}
     fireDetailsDictionary = jsonify(fireDetails.getDetails())
     return fireDetailsDictionary
 
-    
+
+@app.route("/getHospitalList", methods =['GET'])
+def getHospitalDetails():
+    hospitalDetailsDictionary = {}
+    hospitalDetailsDictionary = jsonify(hospitalDetails.getDetails())
+    return hospitalDetailsDictionary
+
+@app.route("/getGardaList", methods =['GET'])
+def getGardaDetails():
+    gardaDetailsDictionary = {}
+    gardaDetailsDictionary = jsonify(gardaDetails.getDetails())
+    return gardaDetailsDictionary
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
---
+++ Back-End/gardaDetails.py
@@ -0,0 +1,39 @@
+import database
+
+def getDetails():
+    gardaDictionary = {}
+    gardaArray = []
+    dbconn = database.db
+    cursor = dbconn.cursor()
+    query = "select * from gardaDetails;"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    for row in data:
+        Name = row[0]
+        Address1 = row[1]
+        Address2 = row[2]
+        Address3 = row[3]
+        Phone = row[4]
+        Website = row[5]
+        Division = row[6]
+        Divisional_HQ = row[7]
+        Divisional_HQ_Phone = row[8]
+        District = row[9]
+        District_HQ = row[10]
+        District_HQ_Phone = row[11]
+        Opening_Hours = row[12]
+        latitude = row[13]
+        longitude = row[14]
+        gardaDictionary = {**gardaDictionary,**{"Name":Name, "Address1":Address1, "Address2":Address2,"Address3":Address3, "Phone":Phone, "Website":Website, "Division":Division,"Divisional_HQ":Divisional_HQ,"Divisional_HQ_Phone":Divisional_HQ_Phone,"District":District,"District_HQ":District_HQ,"District_HQ_Phone":District_HQ_Phone,"Opening_Hours":Opening_Hours,"latitude":latitude,"longitude":longitude}}
+        gardaArray.append(gardaDictionary)
+        
+    return formatJSON(gardaArray,data)
+
+def formatJSON(gardaArray,data):
+    temp = {}
+    if data ==[]:
+        temp["code"] = 400
+    else:
+        temp["code"] = 200
+    temp["data"] = gardaArray
+    return temp
